c4fabde3fbb8f35e98933bcfce2945ece4469828
Kayla Wilson <kaylawilson@wifi_stu-10-38-83-98.uncc.edu>
2018-08-25 11:55:53 -0400

    Updated proejct

---
+++ server.js
@@ -0,0 +1,57 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var React = require("react");
+var ReactDOM = require("react-dom");
+var mongoose = require("mongoose");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Static directory
+app.use(express.static("app/public"));
+
+// Playing with directories in here.
+// This should get us to the first page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/index.html"))
+})
+
+// And this to the second
+app.get("/appointment", function(req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/appointment.html"))
+})
+
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
+
+// React Components
+// ============================
+// class MyComponent extends React.Component {
+//   render() {
+//     return <div> Hello World </div>;
+//   }
+// }
+
+// React.DOM.render(<MyComponent />, node);
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
